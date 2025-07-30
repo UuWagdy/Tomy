0aa53925f2f4e27c7fa1bbf3ef7760ba33110e53
UuWagdy <youssabwagdy123@gmail.com>
2025-07-30 20:50:03 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyA2ag4E5xN46wj85EmGvBYdllOHrrLu1I8",
   authDomain: "tomy-barber-shop.firebaseapp.com",
@@ -9,6 +17,10 @@ const firebaseConfig = {
   appId: "1:693769920483:web:88a3b6cf7318263c540ad6",
   measurementId: "G-HNW5F8YJE3"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
     firebase.initializeApp(firebaseConfig);
     const db = firebase.database();
